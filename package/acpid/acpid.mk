0ddf6550898eea895844ff64c54e8bd3435b0902
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-15 20:30:47 +0200

    acpid: Use new FOO_INIT_SYSV_FILES variable
    With the update of pkg-generic, we don't need to define the
    FOO_INSTALL_INIT_SYSV function.
    In its place, we need to define FOO_INIT_SYSV_FILES with the full path
    to the init script.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -9,11 +9,7 @@ ACPID_SOURCE = acpid-$(ACPID_VERSION).tar.xz
 ACPID_SITE = http://downloads.sourceforge.net/project/acpid2
 ACPID_LICENSE = GPLv2+
 ACPID_LICENSE_FILES = COPYING
-
-define ACPID_INSTALL_INIT_SYSV
-	$(INSTALL) -D -m 0755 package/acpid/S02acpid \
-		$(TARGET_DIR)/etc/init.d/S02acpid
-endef
+ACPID_INIT_SYSV_FILES = package/acpid/S02acpid
 
 define ACPID_SET_EVENTS
 	mkdir -p $(TARGET_DIR)/etc/acpi/events
